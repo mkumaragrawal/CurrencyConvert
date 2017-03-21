cabc36acdddafcb58079f05197e1b40de1e363fd
Manish Agrawal <mkumaragrawal@agl.com.au>
2017-03-21 17:41:36 +0530

    Upload

---
+++ Scripts/currency.js
@@ -0,0 +1,16 @@
+﻿angular.module('Convert', [])
+  .controller('currConverter', ['$scope', '$http', function ($scope, $http) {
+      $scope.rates = {};
+      $http.get('http://api.fixer.io/latest?base=INR&symbols=USD,CAD,EUR')
+        .then(function (res) {
+            $scope.rates = res.data.rates;
+            $scope.toType = $scope.rates.EUR;
+            $scope.fromType = $scope.rates.USD;
+            $scope.fromValue = 0.00;
+            $scope.currConvert();
+        });
+      $scope.currConvert = function () {
+          $scope.toValue = $scope.fromValue * ($scope.toType * (1 / $scope.fromType));
+          $scope.toValue = $scope.toValue;
+      };
+  }])
