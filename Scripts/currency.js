bc5d3bebb1a3adbc8fb0c9c23f553b3c02c6a0c7
Manish Agrawal <mkumaragrawal@agl.com.au>
2017-03-22 11:58:03 +0530

    Update currency.js

--- Scripts/currency.js
+++ Scripts/currency.js
@@ -16,5 +16,5 @@
   }]);
 function isNumberKey(evt){
     var charCode = (evt.which) ? evt.which : event.keyCode;
-    return !(charCode > 31 && (charCode < 46 || charCode > 57));
+    return !(charCode < 46 || charCode > 57 || charCode == 47);
 }
