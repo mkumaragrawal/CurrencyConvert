a0ed2cbb5e43dce8734f7f5176f5d0052349f1db
Manish Agrawal <mkumaragrawal@agl.com.au>
2017-03-23 14:51:41 +0530

    change

--- Scripts/currency.js
+++ Scripts/currency.js
@@ -4,8 +4,8 @@
       $http.get('http://api.fixer.io/latest?base=INR&symbols=USD,CAD,EUR')
         .then(function (res) {
             $scope.rates = res.data.rates;
-            $scope.toType = $scope.rates.EUR;
-            $scope.fromType = $scope.rates.USD;
+            $scope.toType = $scope.rates.USD;
+            $scope.fromType = $scope.rates.CAD;
             $scope.fromValue = 0.00;
 		$scope.myVar = false;
             $scope.currConvert();
@@ -16,10 +16,34 @@
     		};
       $scope.currConvert = function () {
           $scope.toValue = $scope.fromValue * ($scope.toType * (1 / $scope.fromType));
-          $scope.toValue = $scope.toValue;
+          $scope.toValue = $scope.toValue.toFixed(2);;
       };
   }]);
-function isNumberKey(evt){
+function validateFloatKeyPress(el, evt) {
     var charCode = (evt.which) ? evt.which : event.keyCode;
-    return !(charCode < 46 || charCode > 57 || charCode == 47);
+    var number = el.value.split('.');
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    //just one dot
+    if(number.length>1 && charCode == 46){
+         return false;
+    }
+    //get the carat position
+    var caratPos = getSelectionStart(el);
+    var dotPos = el.value.indexOf(".");
+    if( caratPos > dotPos && dotPos>-1 && (number[1].length > 1)){
+        return false;
+    }
+    return true;
+}
+
+//thanks: http://javascript.nwbox.com/cursor_position/
+function getSelectionStart(o) {
+	if (o.createTextRange) {
+		var r = document.selection.createRange().duplicate()
+		r.moveEnd('character', o.value.length)
+		if (r.text == '') return o.value.length
+		return o.value.lastIndexOf(r.text)
+	} else return o.selectionStart
 }
